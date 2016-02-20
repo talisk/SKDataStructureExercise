087cbd5a38bc98ac802af007c21de00a8a401af6
talisk <talisk@foxmail.com>
2016-02-20 10:39:45 +0800

    Add ex006

---
+++ ex006/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef COUNT
+#define COUNT 10
+#endif
+
+int numbers[COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+int countI(int number[], int i) {
+    int k = 0;
+    for (int j = 0; j < COUNT; ++j)
+        (number[j] == i)?(k++):(k);
+    return k;
+}
+
+int adjust(int number[]) {
+    int modifyFlag = 0;
+    for (int i = 0; i < COUNT; ++i) {
+        if (number[i] != countI(number, i)) modifyFlag = 1;
+        number[i] = countI(number, i);
+    }
+    return modifyFlag;
+}
+
+int main(int argc, char const *argv[]) {
+
+    while (adjust(numbers)) {
+        for (int j = 0; j < COUNT; ++j) {
+            cout << numbers[j] << ",";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
